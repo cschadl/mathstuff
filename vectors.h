b87adb9faf8aea57da5742cc76ee6cdb2b545fd8
Chris Schadl <cschadl@gmail.com>
2016-09-24 12:39:01 -0500

    Changed an int to a size_t

--- vectors.h
+++ vectors.h
@@ -329,7 +329,7 @@ template <typename U, int M>
 inline std::ostream& operator<<(std::ostream& os, const n_vector<U, M>& v)
 {
 	os << "( ";
-	for (int i = 0 ; i < v.size() ; i++)
+	for (size_t i = 0 ; i < v.size() ; i++)
 		os << v[i] << " ";
 	os << ")";
 
